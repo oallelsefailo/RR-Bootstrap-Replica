aace05fac8d08f3ef3ad2c76ad1a7b389074dade
Brandon <oallelsefailo@gmail.com>
2023-08-28 07:28:40 -0700

    Need to pause

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import TopBar from "./components/TopBar";
+import Background from "./components/Background";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <Background />
     </div>
   );
 }
